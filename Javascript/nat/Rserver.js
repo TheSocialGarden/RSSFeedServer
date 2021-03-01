27fa3da357fa64e94b7675923fda609c94e418ad
Kysetopher <ckyser2@gmail.com>
2021-03-01 14:54:45 -0800

    Reformating
    RSS Website reformatting to make images fit

--- Javascript/nat/Rserver.js
+++ Javascript/nat/Rserver.js
@@ -14,19 +14,25 @@ console.log($.ajax(RSS_URL, {
         const el = $(this);
 
         const template = `
-          <article>
-            <img src="${el.find("image").find("url").text()}" alt="">
-            <h2>
-              <a href="${el
-                .find("link")
-                .text()}" target="_blank" rel="noopener">
-                ${el.find("title").text()}
-              </a>
-            </h2>
-			<p>
-                ${el.find("description").text()}
-			</p>
-          </article>
+          <article><table>
+            <tr>
+				<td>
+				<h2>
+					<a href="${el
+					.find("link")
+					.text()}" target="_blank" rel="noopener">
+					${el.find("title").text()}
+					</a>
+				</h2>
+				<p>
+					${el.find("description").text()}
+				</p>
+				</td>
+				
+				<td><img src="${el.find("image").find("url").text()}" alt=""></td>
+			</tr>
+			</table>
+          </table></article>
         `;
 
         document.body.insertAdjacentHTML("beforeend", template);
